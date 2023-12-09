bf272b2e198f8142e8727c4776e48aa9a2203749
anatolii_filipenko <anatoliy.fil033@gmail.com>
2023-12-09 19:13:32 +0200

    article creation, articles middleware

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,14 +1,33 @@
-const articles = [{
-    name: 'Express - introduction',
-    description: 'Express - text',
-    type: 'a',
-    tags: []
-}]
+const articles = [
+    {
+        name: 'Express - introduction',
+        description: 'Express - text',
+        type: 'a',
+        tags: []
+    },
+    {
+        name: 'Something1 - introduction',
+        description: 'something1 - text',
+        type: 'b',
+        tags: []
+    },
+    {
+        name: 'Something2 - introduction',
+        description: 'Something2 - text',
+        type: 'a',
+        tags: []
+    }
+]
 
 function getArticles(req, res) {
     res.json(articles)
 }
+function createArticle(req, res) {
+    articles.push(req.body)
+    res.status(201).json(req.body)
+}
 
 module.exports = {
-    getArticles
+    getArticles,
+    createArticle
 }
--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const articlesController = require('../controllers/articles')
-
+const articlesMdware = require('../middlewares/articles.mdware')
 const router = express.Router()
 
 router.get('/', articlesController.getArticles)
+router.post('/', articlesMdware.validateBody, articlesController.createArticle)
 
 module.exports = router
