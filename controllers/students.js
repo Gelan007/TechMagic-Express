a75308980398d322626b636c9f929b8bc4d908c5
anatolii_filipenko <anatoliy.fil033@gmail.com>
2023-12-09 18:48:28 +0200

    Get user with worst homework score

--- controllers/students.js
+++ controllers/students.js
@@ -1,25 +1,80 @@
-const studentStatistics = [{
-    "name": "Aimee Zank",
-    "scores": [
-        {
-            "score": 1.463179736705023,
-            "type": "exam"
-        },
-        {
-            "score": 11.78273309957772,
-            "type": "quiz"
-        },
-        {
-            "score": 35.8740349954354,
-            "type": "homework"
-        }
-    ]
-}]
+const studentStatistics = [
+    {
+        "name": "Aimee Zank",
+        "scores": [
+            {
+                "score": 1.463179736705023,
+                "type": "exam"
+            },
+            {
+                "score": 11.78273309957772,
+                "type": "quiz"
+            },
+            {
+                "score": 35.8740349954354,
+                "type": "homework"
+            }
+        ]
+    },
+    {
+        "name": "Ivan Li",
+        "scores": [
+            {
+                "score": 11.463179736705023,
+                "type": "exam"
+            },
+            {
+                "score": 31.78273309957772,
+                "type": "quiz"
+            },
+            {
+                "score": 23.8740349954354,
+                "type": "homework"
+            }
+        ]
+    },
+    {
+        "name": "Alice Smith",
+        "scores": [
+            {
+                "score": 21.463179736705023,
+                "type": "exam"
+            },
+            {
+                "score": 33.78273309957772,
+                "type": "quiz"
+            },
+            {
+                "score": 34.8740349954354,
+                "type": "homework"
+            }
+        ]
+    },
+
+]
 
 function getStudentsStatistics(req, res) {
     res.json(studentStatistics)
 }
 
+function getStudentWithWorstHomeworkScore(req, res) {
+    let worstScore = Infinity
+    let worstStudent;
+
+    for (let student of studentStatistics) {
+        let homeworkScore = student.scores.find(s => s.type === 'homework').score;
+        if (homeworkScore < worstScore) {
+            worstStudent = student
+            worstScore = homeworkScore;
+        }
+    }
+    if(!worstStudent) {
+        return res.status(404).json({message: `Not a single student was found who has some scores with type "homework"`})
+    }
+    return res.json(worstStudent);
+}
+
 module.exports = {
-    getStudentsStatistics
+    getStudentsStatistics,
+    getStudentWithWorstHomeworkScore
 }
